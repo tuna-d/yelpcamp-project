646a84d82c5a0e4bf2340eba99ad734bf443c04e
tuna-d <tdemirel98@gmail.com>
2025-04-23 18:03:59 +0300

    install helmet and set up content security policy

--- app.js
+++ app.js
@@ -14,10 +14,12 @@ const flash = require("connect-flash")
 const passport = require("passport")
 const passportLocalStrategy = require("passport-local")
 const User = require("./models/user")
+const helmet = require("helmet")
 
 const campgroundRoutes = require("./routes/campgroundRoutes")
 const reviewRoutes = require("./routes/reviewRoutes")
 const userRoutes = require("./routes/userRoutes")
+const { contentSecurityPolicy } = require("helmet")
 
 mongoose.connect("mongodb://127.0.0.1:27017/yelpCamp")
 
@@ -38,12 +40,60 @@ app.use(bodyParser.json())
 app.use(mongoSanitize())
 app.use(methodOverride("_method"))
 app.use(express.static(path.join(__dirname, "/public")))
+app.use(helmet())
+
+const scriptSrcUrls = [
+  "https://stackpath.bootstrapcdn.com/",
+  "https://api.tiles.mapbox.com/",
+  "https://api.mapbox.com/",
+  "https://kit.fontawesome.com/",
+  "https://cdnjs.cloudflare.com/",
+  "https://cdn.jsdelivr.net",
+]
+const styleSrcUrls = [
+  "https://kit-free.fontawesome.com/",
+  "https://stackpath.bootstrapcdn.com/",
+  "https://api.mapbox.com/",
+  "https://api.tiles.mapbox.com/",
+  "https://fonts.googleapis.com/",
+  "https://use.fontawesome.com/",
+  "https://cdn.jsdelivr.net/",
+]
+const connectSrcUrls = [
+  "https://api.mapbox.com/",
+  "https://a.tiles.mapbox.com/",
+  "https://b.tiles.mapbox.com/",
+  "https://events.mapbox.com/",
+]
+const fontSrcUrls = []
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: [],
+      connectSrc: ["'self'", ...connectSrcUrls],
+      scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+      styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+      workerSrc: ["'self'", "blob:"],
+      objectSrc: [],
+      imgSrc: [
+        "'self'",
+        "blob:",
+        "data:",
+        "https://res.cloudinary.com/dirdof2ca/",
+        "https://images.unsplash.com/",
+      ],
+      fontSrc: ["'self'", ...fontSrcUrls],
+    },
+  })
+)
 
 const sessionConfig = {
+  name: "dln_ss",
   secret: "yelpCampSecret",
   resave: false,
   saveUninitialized: true,
   httpOnly: true,
+  //secure: true,
   cookie: {
     expires: Date.now() + 1000 * 3600 * 24 * 7,
     maxAge: 1000 * 3600 * 24 * 7,
